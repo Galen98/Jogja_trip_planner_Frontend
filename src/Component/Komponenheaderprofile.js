7df4126117765e0a41d739deb10b6e6d2193fb5c
Macbook <macbook@Macbooks-MacBook-Air.local>
2023-09-26 14:04:52 +0700

    baru4

--- src/Component/Komponenheaderprofile.js
+++ src/Component/Komponenheaderprofile.js
@@ -8,7 +8,7 @@ function Komponenheaderprofile(){
 <div className='rounded-top text-white d-flex flex-row headerprofile' style={{height:"200px"}}> 
 <div className='ms-4 mt-5 d-flex flex-column' style={{width:"150px"}}>
 {isImage ? (
-    <img src={"http://203.194.113.182/storage/img/"+user.image} className='img-fluid img-thumbnail mb-5' style={{width:"150px", zIndex:"1"}}/>
+    <img src={"https://203.194.113.182/storage/img/"+user.image} className='img-fluid img-thumbnail mb-5' style={{width:"150px", zIndex:"1"}}/>
     
 ) : (
     <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" className='img-fluid img-thumbnail mt-4 mb-2' style={{width:"150px", zIndex:"1", borderRadius: "100px"}}/>
